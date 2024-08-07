ee78e8af57a09e98fe1e458468b7cb93b17345b9
Rinae Hyun <rnhyun.dream@gmail.com>
2024-08-07 12:52:26 +0200

    Replace grades with German- and AmericanGrades types

--- src/task/printstudentinfo/studentType.ts
+++ src/task/printstudentinfo/studentType.ts
@@ -3,5 +3,8 @@ export type Student = {
     lastname: string,
     age: number,
     // Step 2: allow string and numbers as grades
-    grades: (string | number | undefined)[]
+    grades: (GermanGrades | AmericanGrades | undefined)[]
 }
+
+type GermanGrades = 1 | 2 | 3 | 4 | 5 | 6;
+type AmericanGrades = "A" | "B" | "C" | "D" | "E" | "F";
